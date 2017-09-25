1a0760a5de3df35a819f59c060e3a369ebf52166
bowersad <bowersad@gmail.com>
2017-09-25 08:57:55 -0400

    Building Models and Entities

---
+++ CandlePOCAPI/CandlePOCAPI/Models/BaseResource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandlePOCAPI.Models
+{
+    public abstract class BaseResource
+    {
+        public string href { get; set; }
+    }
+}
---
+++ CandlePOCAPI/CandlePOCAPI/Models/Scent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandlePOCAPI.Models
+{
+    public class Scent : BaseResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PrimaryNote { get; set; }
+
+        public string SecondaryNote { get; set; }
+
+        public string BaseNote { get; set; }
+    }
+}
