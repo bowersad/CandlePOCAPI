46939bb95c6f28c71f822d99b2bc93b17550bd9b
bowersad <bowersad@gmail.com>
2017-09-25 09:10:48 -0400

    Build Context Layer

---
+++ CandlePOCAPI/CandlePOCAPI/CandlePOCContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandlePOCAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandlePOCAPI
+{
+    public class CandlePOCContext : DbContext
+    {
+        public static string ConnectionString { get; set; }
+
+        public CandlePOCContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<ScentEntity> Scents { get; set; }
+    }
+}
--- CandlePOCAPI/CandlePOCAPI/Models/ScentEntity.cs
+++ CandlePOCAPI/CandlePOCAPI/Models/ScentEntity.cs
@@ -7,5 +7,14 @@ namespace CandlePOCAPI.Models
 {
     public class ScentEntity
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PrimaryNote { get; set; }
+
+        public string SecondaryNote { get; set; }
+
+        public string BaseNote { get; set; }
     }
 }
